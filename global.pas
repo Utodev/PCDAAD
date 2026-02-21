1769d06eb20bdf0ec1c2efef0abde846d9db6489
Uto <utodev@gmail.com>
2026-02-21 20:01:17 +0100

    version updated to 1.13 Gates of Mirkwood

--- global.pas
+++ global.pas
@@ -6,7 +6,7 @@ unit GLOBAL;
 
 interface
 
-const VERSION='1.12 - Samudio''s Edition';
+const VERSION='1.13 - Gates of Mirkwood';
       NO_WORD = $FF;
       END_OF_PROCESS_MARK = $00;
       END_OF_CONDACTS_MARK = $FF;
